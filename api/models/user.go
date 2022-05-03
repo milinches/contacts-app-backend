aea8f77384c4ca96f43f419eb03ff63bdb49550e
milinches <ibrogenius5165@gmail.com>
2022-05-03 16:13:25 +0100

    added user model

--- api/models/user.go
+++ api/models/user.go
@@ -1,1 +1,21 @@
 package models
+
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
+
+type (
+	User struct {
+		gorm.Model
+		ID uint
+		Username string
+		Email *string
+		FirstName string
+		LastName string
+		Password string
+		CreatedAt time.Time
+		UpdatedAt time.Time
+	}
+)
